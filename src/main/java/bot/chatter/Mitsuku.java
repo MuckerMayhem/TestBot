4942f000c596ddc1a40b4c990e215a54f5b7e807
wiizerdofwiierd <imtoby@comcast.net>
2016-06-16 20:17:31 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/main/java/bot/DiscordBot.java

---
+++ src/main/java/bot/chatter/Mitsuku.java
@@ -0,0 +1,36 @@
+package bot.chatter;
+
+import bot.DiscordBot;
+import sx.blah.discord.api.events.EventSubscriber;
+import sx.blah.discord.handle.impl.events.MessageReceivedEvent;
+import sx.blah.discord.util.DiscordException;
+import sx.blah.discord.util.MessageBuilder;
+import sx.blah.discord.util.MissingPermissionsException;
+import sx.blah.discord.util.RateLimitException;
+
+/**
+ * Created by Owner on 2016-06-16.
+ */
+public class Mitsuku
+{
+    @EventSubscriber
+    public void onMessageReceived(MessageReceivedEvent event) throws RateLimitException, DiscordException, MissingPermissionsException
+    {
+        MessageBuilder builder = new MessageBuilder(DiscordBot.instance.getClient());
+        if(event.getMessage().getChannel().getName().equals("bot"))
+        {
+            builder.withChannel(event.getMessage().getChannel());
+            String s = event.getMessage().getContent();
+            try
+            {
+                s = DiscordBot.session.think(s);
+                builder.appendContent(s).build();
+
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
--- src/main/java/bot/commands/sound/Sound.java
+++ src/main/java/bot/commands/sound/Sound.java
@@ -4,7 +4,8 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public enum Sound{
+public enum Sound
+{
 
     QUACK("quack", new File("C:\\Users\\wiize\\Downloads\\quack.mp3"), "duck"),
     BOOTY("booty", new File("C:\\Users\\wiize\\Downloads\\booty.mp3"), "(͡°͜ʖ͡°)"),
@@ -12,8 +13,16 @@ public enum Sound{
     LOL("lol", new File("C:\\Users\\wiize\\Downloads\\lol.mp3")),
     SOGOOD("sogood", new File("C:\\Users\\wiize\\Downloads\\sogood.mp3")),
     ALLAHU("allahuakbar", new File("C:\\Users\\wiize\\Downloads\\allahuakbar.mp3"), "jihad", "allahu"),
-    YEAH("raymanyeah", new File("C:\\Users\\Owner\\IdeaProjects\\Test Bot\\raymanyeah.mp3")),
-    DUMMY("dummy", new File("C:\\Users\\wiize\\Downloads\\dummy.mp3"));
+    YEAH("raymanyeah", new File("../raymanyeah.mp3")),
+    DUMMY("dummy", new File("../dummy.mp3")),
+    BALLSOFSTEEL("ballsofsteel", new File("../ballsofsteel.mp3")),
+    BALLS("balls", new File("../balls.mp3")),
+    MACHO1("circle", new File("../macho_alert16.mp3")),
+    MACHO2("getyou", new File("../macho_alert13.mp3")),
+    ALIVE("alive", new File("../alive.mp3")),
+    BUBBERDUCK("bubberduck", new File("../bubberduck.mp3")),
+    BONETROUSLE("bones", new File("../bonetrousle.mp3")),
+    BONES("bonel", new File("../bones.mp3"));
 
     private String name;
     private String[] aliases;
@@ -21,37 +30,52 @@ public enum Sound{
     private String url;
     private File path;
 
-    Sound(String name, String url, String... aliases){
-        this.name = name; this.url = url; this.aliases = aliases;
+    Sound(String name, String url, String... aliases)
+    {
+        this.name = name;
+        this.url = url;
+        this.aliases = aliases;
     }
 
     @Deprecated
-    Sound(String name, File path, String... aliases){
-        this.name = name; this.aliases = aliases; this.path = path;
+    Sound(String name, File path, String... aliases)
+    {
+        this.name = name;
+        this.aliases = aliases;
+        this.path = path;
     }
 
-    public static Sound get(String name){
-        for(Sound s : values()){
-            if(s.getName().equalsIgnoreCase(name)) return s; for(String a : s.aliases){
-                if(a.equalsIgnoreCase(name)) return s;
+    public static Sound get(String name)
+    {
+        for (Sound s : values())
+        {
+            if (s.getName().equalsIgnoreCase(name)) return s;
+            for (String a : s.aliases)
+            {
+                if (a.equalsIgnoreCase(name)) return s;
             }
-        } return null;
+        }
+        return null;
     }
 
-    public String getName(){
+    public String getName()
+    {
         return this.name;
     }
 
-    public String[] getAliases(){
+    public String[] getAliases()
+    {
         return this.aliases;
     }
 
-    public URL getUrl() throws MalformedURLException{
+    public URL getUrl() throws MalformedURLException
+    {
         return new URL(this.url);
     }
 
     @Deprecated
-    public File getPath(){
+    public File getPath()
+    {
         return this.path;
     }
 }
